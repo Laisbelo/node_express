d5d7dd1f46f771e4696fc1ffd5b6d42fa7c93333
Laisbelo <lais_b21@hotmail.com>
2023-04-14 10:21:56 -0300

    Atividade 2

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const alunos = require("./alunos");
 
-
+//ROTA GET ALUNOS
 app.get("/alunos",(req,res)=>{
     let listaAlunos = alunos.alunos;
     if(req.query.nome){
@@ -14,6 +16,18 @@ app.get("/alunos",(req,res)=>{
     } 
     res.json(listaAlunos);
 });
+
+//ROTA POST ALUNOS
+app.post("/alunos/novo",(req,res)=>{
+    let {nome,matricula,media} = req.body;
+    if(nome != undefined && matricula != undefined && media != undefined) {
+        res.json(`Nome: ${nome}, Matricula: ${matricula} e Media: ${media}`)
+    } else { 
+        res.status(400).json("Exitem campos vazios ou incorretos")
+    }
+});
+
+
 
 
 app.listen(3000,()=>{
